9959595ff44f58a7a7db080fcf0728d08626bc36
Ezra <ezrakissous26@gmail.com>
2026-08-23 12:46:07 +0300

    create first structure of routes and repo functions

---
+++ server/repo/game-repo.js
@@ -0,0 +1,74 @@
+import { client } from "../db/connect";
+
+
+async function initGame() {
+    try {
+        const result = await
+        return result
+    } catch (error) {
+        console.error('Error :', error.message)
+    }
+}
+
+async function createRound() {
+    try {
+        const result = await
+        return result
+    } catch (error) {
+        console.error('Error :', error.message)
+    }
+}
+
+async function createPLayer() {
+    try {
+        const result = await
+        return result
+    } catch (error) {
+        console.error('Error :', error.message)
+    }
+}
+
+async function init() {
+    try {
+        const result = await
+        return result
+    } catch (error) {
+        console.error('Error :', error.message)
+    }
+}
+
+async function getPlayerById() {
+    try {
+        const result = await
+        return result
+    } catch (error) {
+        console.error('Error :', error.message)
+    }
+}
+
+async function getGameById() {
+    try {
+        const result = await
+        return result
+    } catch (error) {
+        console.error('Error :', error.message)
+    }
+}
+
+async function updateMap() {
+    try {
+        const result = await
+        return result
+    } catch (error) {
+        console.error('Error :', error.message)
+    }
+}
+
+async function updateSoldierNumber() {
+    try {
+        const result = await
+        return result
+    } catch (error) {
+        console.error('Error :', error.message)
+    }
+}
--- server/routes/route.js
+++ server/routes/route.js
@@ -2,6 +2,30 @@ import express from 'express'
 
 export const router =  express.Router()
 
-router.get('/', (req, res) => {
-    res.status(200).json({message: 'hello world'})
+// router.get('/', (req, res) => {
+//     res.status(200).json({message: 'hello world'})
+// })
+
+router.post('/games', (req, res) => {
+    res.send('banana')
+})
+
+router.get('/games/:id', (req, res) => {
+    res.send('banana')
+})
+
+router.post('/games/:id/reinforce', (req, res) => {
+    res.send('banana')
+})
+
+router.post('/games/:id/attack', (req, res) => {
+    res.send('banana')
+})
+
+router.post('/games/:id/move', (req, res) => {
+    res.send('banana')
+})
+
+router.post('/games/:id/end-turn', (req, res) => {
+    res.send('banana')
 })
