69db4200bf547b745dc9db5d5fe0ee6b053cc784
Ezra <ezrakissous26@gmail.com>
2026-08-24 17:20:45 +0300

    add dependencie CORS in pacjage.json and in server.js

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv/config'
 import { router } from './routes/route.js'
 import { clientConnect } from './db/connect.js'
+import cors from 'cors'
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/', router)
